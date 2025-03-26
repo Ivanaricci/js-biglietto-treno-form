3047e71f1c619eb15251fecce90e2e810c643681
Ivanaricci <ivana.ricci26@gmail.com>
2025-03-26 15:43:44 +0100

    [UPD] Add input e recupero in js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+
+
+
+// Recupero dal DOM dei tag input e dichiarazione variabili
+
+const km = document.getElementById('km')
+const age = document.getElementById('age')
+const enter = document.getElementById('enter')
+
+let prezzo = km * 0.21;
+let messaggio;
+
+// Corpo del programma
+
+
