3758328709ab7ece38b2b0e9ba2677b806ec1b12
Ivanaricci <ivana.ricci26@gmail.com>
2025-03-26 16:27:16 +0100

    [UPD] Add valore inserito dall'utente in console

--- js/script.js
+++ js/script.js
@@ -4,13 +4,25 @@
 
 // Recupero dal DOM dei tag input e dichiarazione variabili
 
-const km = document.getElementById('km')
-const age = document.getElementById('age')
+const km = document.getElementById('km-utente')
+const age = document.getElementById('age-utente')
 const enter = document.getElementById('enter')
 
-let prezzo = km * 0.21;
-let messaggio;
+let price = km * 0.21;
+
 
 // Corpo del programma
 
 
+enter.addEventListener('click', function(){
+
+    let kmUtente = km.value.trim()
+    console.log(kmUtente)
+
+    let ageUtente = age.value.trim()
+    console.log(ageUtente)
+
+    
+    
+
+})
