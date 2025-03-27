85d03ad4694f73cecb265d22f6438abd95488196
Ivanaricci <ivana.ricci26@gmail.com>
2025-03-27 08:24:38 +0100

    [UPD] Fine Milestone2

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 
 
-
-
 // Recupero dal DOM dei tag input e dichiarazione variabili
 
 const km = document.getElementById('km-utente')
 const age = document.getElementById('age-utente')
-const enter = document.getElementById('enter')
+const button = document.querySelector('button')
+
+// Recupero i segnaposto della card
+
+const offerta = document.querySelector('.offerta')
+const costoBiglietto = document.querySelector('.costo-biglietto')
 
 
 
@@ -14,32 +17,37 @@ const enter = document.getElementById('enter')
 // Corpo del programma
 
 
-enter.addEventListener('click', function(){
+button.addEventListener('click', function(event){
+    event.preventDefault();
 
-    let kmUtente = km.value.trim()
+    let kmUtente = parseFloat(km.value.trim())
     console.log(kmUtente)
 
-    let ageUtente = age.value.trim()
+    let ageUtente = parseInt(age.value.trim())
     console.log(ageUtente)
 
     let price = kmUtente * 0.21;
+    let tipoOfferta = 'Biglietto standard'
     
 
     // sconti
     if(ageUtente < 18){
         price = price * 0.8;
-        
+        tipoOfferta = 'Sconto Minorenni 20%'
         console.log(`hai diritto al 20% di sconto. Il prezzo del tuo biglietto è ${price.toFixed(2)} euro`);
     }
 
     else if(ageUtente > 65){
         price = price * 0.6;
-        
+        tipoOfferta = 'Sconto over 65 40%'
         console.log(`hai diritto al 40% di sconto. Il prezzo del tuo biglietto è ${price.toFixed(2)} euro`);
     }
 
-    else{
-        console.log(`Il prezzo del tuo biglietto è ${price} euro`);
-    }
+    // Stampo i dati nella card
+    offerta.innerText = tipoOfferta;
+    costoBiglietto.innerText = price.toFixed(2) + 'euro'
 
+     
+
+       
 })
